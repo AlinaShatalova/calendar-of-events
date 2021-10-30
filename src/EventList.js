01a1a2952dda651880d23698f9de11164f2accb7
AlinaShatalova <lina.shatalova@yandex.ru>
2021-10-30 16:51:57 +0300

    fixed filter

--- src/EventList.js
+++ src/EventList.js
@@ -4,18 +4,37 @@ import './EventList.css'
 
 const EventList = (props) => {
     const { events, isFavActive, favorites, city, month } = props;
-
+    
     const filteredList = () => {
-        const newList = events.filter((event) => {
-            if(event.city === city && Number(event.date.split('.')[1]) === Number(month)) {
-                return true;
-            } return false;
-        })
+        let newList = [];
+        if (city === 'All cities' && month === '13') {
+            newList = events;
+        } else if (city === 'All cities' && month !== '13') {
+            newList = events.filter((event) => {
+                if (Number(event.date.split('.')[1]) === Number(month)) {
+                    return true;
+                } return false;
+            })
+        } else if (city !== 'All cities' && month === '13') {
+            newList = events.filter((event) => {
+                if (event.city === city) {
+                    return true;
+                } return false;
+            })
+        } else {
+            newList = events.filter((event) => {
+                if (event.city === city && Number(event.date.split('.')[1]) === Number(month)) {
+                    return true;
+                } return false;
+            })
+        }
         return newList;
     }
 
+    const filteredEvents = filteredList(); 
+
     const userFavoriteList = () => {
-        const newList = events.filter((event) => {
+        const newList = filteredEvents.filter((event) => {
             if (favorites.includes(Number(event.id))) {
                 return true;
             } return false;
@@ -24,7 +43,6 @@ const EventList = (props) => {
     }
 
     function renderList() {
-        const filteredEvents = filteredList();
         if (!isFavActive && filteredEvents.length > 0) {
             return (
                 <>
@@ -38,6 +56,7 @@ const EventList = (props) => {
             const favoriteList = userFavoriteList();
             return (
                 <>
+                    {favoriteList.length > 0 ? '' : <p className="no-events">Events not found</p>}
                     {favoriteList.map((event) => {
                             return <EventCard key={event.id} event={event} isFav={favorites.includes(Number(event.id)) ? true : false} />                 
                     })}
--- src/FilterList.js
+++ src/FilterList.js
@@ -4,20 +4,17 @@ import './FilterList.css'
 const FilterList = (props) => {
     const { cities, months, onCityChange, onMonthChange, onFavChange, isActive, city, month } = props;
 
-    const handleChangeCity = (e) => onCityChange(e.target.value);
-    const handleChangeMonth = (e) => onMonthChange(e.target.value);
-
     return (
         <div className="filters">
             <label>
                 City:
-                <select className="filters__select" value={city} onChange={handleChangeCity}>
+                <select className="filters__select" value={city} onChange={onCityChange}>
                     {cities.map((c) => <option key={c} value={c}>{c}</option>)}
                 </select>
             </label>
             <label>
                 Months:
-                <select className="filters__select" value={month} onChange={handleChangeMonth}>
+                <select className="filters__select" value={month} onChange={onMonthChange}>
                     {months.map((m) => <option key={m.id} value={m.id}>{m.name}</option>)}
                 </select>
             </label>
